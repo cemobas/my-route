c9be398a8a7a13648ce4687eac1e95143ec2c98b
cemobas <cemonurbas@gmail.com>
2020-08-16 15:01:41 +0200

    Return a new array instead of changing state and add remove route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import C from './constants'
 import { routes } from './initialState.json'
-import { spot } from './store/reducers'
+import { spot, route } from './store/reducers'
 
 const state = {
     "routes": [{
@@ -21,14 +21,21 @@ const addDoruksRoute = {
 	type: C.ADD_ROUTE,
 	payload: {"_id": "11", "name": "DoruksRoute", "author": "doruk", "spots": [{"name": "SKY BAR", "order": 99}]}
 }
+const removeCemsRoute = {
+	type: C.REMOVE_ROUTE,
+	payload: {"_id": "1"}
+}
 
-const nextStateAfterAddingSkyBar = spot(state, addSkyBarAction)
-const nextStateAfterAddingDoruksRoute = spot(state, addDoruksRoute)
+const nextStateAfterAddingSkyBar = spot(undefined, addSkyBarAction)
+const nextStateAfterAddingDoruksRoute = route(state.routes, addDoruksRoute)
+const nextStateAfterRemovingCemsRoute = route(state.routes, removeCemsRoute)
 
 console.log(`
-    initial goal: ${state}
+    initial state: ${JSON.stringify(state)}
     addSkyBarAction: ${JSON.stringify(addSkyBarAction)}
-    addDoruksRoute: ${JSON.stringify(addSkyBarAction)}
+    addDoruksRoute: ${JSON.stringify(addDoruksRoute)}
+    removeCemsRoute: ${JSON.stringify(removeCemsRoute)}
     new goal 1: ${JSON.stringify(nextStateAfterAddingSkyBar)}
     new goal 2: ${JSON.stringify(nextStateAfterAddingDoruksRoute)}
+    new goal 3: ${JSON.stringify(nextStateAfterRemovingCemsRoute)}
 `)
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,20 +2,25 @@ import C from '../constants'
 
 export const spot = (state=[{"name": "Unknown spot", "order": 0}], action) => {
 
-   if (action.type === C.ADD_SPOT) {
-       state.push(action.payload)
-   }
+    if (action.type === C.ADD_SPOT) {
+       return [...state, action.payload] // return a new array instead of changing state
+    }
 
-   return state
+    return state
 
 }
 
-export const route = (state={"_id": "0", "name": "foobar route", "author": "foobar", "spots": [{"name": "FOO BAR", "order": 13}]}, action) => {
+export const route = (state=[{"_id": "0", "name": "foobar route", "author": "foobar", "spots": [{"name": "FOO BAR", "order": 13}]}], action) => {
 
-   if (action.type === C.ADD_ROUTE) {
-        state.push(action.payload)
-   }
-   
-   return state
+    switch(action.type) {
+        case C.ADD_ROUTE:
+            return [...state, action.payload] // return a new array instead of changing state
+        
+        case C.REMOVE_ROUTE:
+            return state.filter((r, i) => r._id !== action.payload._id)
+        
+        default:
+            return state
+    }
 
 }
